ea2814b2d298b5d2820c96188daef5bbeaf26f22
nurbolatk <nurbolattk@gmail.com>
2021-02-05 23:50:55 +0600

    Add input that controls cube's rotation

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 const btnOpenBox = document.querySelector('.open-box')
 const cube = document.querySelector('.cube')
+const rotation = document.getElementById('rotation')
 
 btnOpenBox.addEventListener('click', () => {
   cube.classList.toggle('open')
+})
+
+rotation.addEventListener('input', e => {
+  cube.style.transform = `translateZ(-100px) rotateX(-20deg) rotateY(${
+    parseInt(e.target.value) - 30
+  }deg)`
 })
